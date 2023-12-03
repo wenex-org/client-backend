df15691b84a91f0e68a47ea00f6a491f43554a30
Vahid V. <vhid.vz@gmail.com>
2023-12-03 21:12:01 +0330

    update

--- apps/services/src/modules/public/public.service.ts
+++ apps/services/src/modules/public/public.service.ts
@@ -9,34 +9,28 @@ export class PublicService {
   async getHost(id: string, headers?: Headers) {
     const app = await this.sdkService.client.domain.apps.findById(id, {
       headers,
-      params: { zone: 'client' },
+      params: {
+        projection: 'id cid url site type name logo status slogan version created_at',
+        zone: 'client',
+      },
     });
 
     const client = (
       await this.sdkService.client.domain.clients.find(
         {
-          query: { $or: [{ id }, { client_id: id }] },
+          query: { $or: [app.cid ? { id: app.cid } : { id }, { client_id: id }] },
         },
         {
           headers,
           params: {
-            projection: {
-              status: 1,
-              state: 1,
-              logo: 1,
-              name: 1,
-              slogan: 1,
-              created_at: 1,
-              'domains.name': 1,
-              'domains.status': 1,
-              'domains.subjects': 1,
-            },
+            projection:
+              'id url plan site logo name state status slogan created_at domains.name domains.status domains.subjects',
             zone: 'client',
           },
         },
       )
     )?.pop();
 
-    return { app, client };
+    return app.id ? { app, client } : { client };
   }
 }
