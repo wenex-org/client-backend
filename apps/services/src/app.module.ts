4ebcb38d5cbd19e9af32a3cc2a94806f33958087
Vahid V. <vhid.vz@gmail.com>
2023-12-24 21:09:17 +0330

    update

--- apps/services/src/app.module.ts
+++ apps/services/src/app.module.ts
@@ -1,6 +1,7 @@
-import { REDIS_CONFIG, SENTRY_CONFIG } from '@app/common/configs';
+import { MONGO_CONFIG, REDIS_CONFIG, SENTRY_CONFIG } from '@app/common/configs';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { SentryModule } from '@ntegral/nestjs-sentry';
+import { MongooseModule } from '@nestjs/mongoose';
 import { HealthModule } from '@app/health';
 import { RedisModule } from '@app/redis';
 import { Module } from '@nestjs/common';
@@ -16,9 +17,11 @@ import { PublicModule } from './modules/public';
       timeout: +(process.env.TIMEOUT || 30000),
       headers: { 'x-api-key': process.env.API_KEY },
     }),
+
     PrometheusModule.register(),
     RedisModule.forRoot(REDIS_CONFIG()),
     SentryModule.forRoot(SENTRY_CONFIG()),
+    MongooseModule.forRoot(MONGO_CONFIG()),
     HealthModule.forRoot(['disk', 'memory', 'redis']),
 
     ...[AuthModule, PublicModule],
