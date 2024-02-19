3915d1708625b0ba4fd764c92fd3aa510af3a751
Vahid V. <vhid.vz@gmail.com>
2024-02-19 12:40:08 +0330

    update

--- apps/services/src/modules/mails/mails.controller.ts
+++ apps/services/src/modules/mails/mails.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, UseFilters, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  UseFilters,
+  UseGuards,
+  UseInterceptors,
+  UsePipes,
+} from '@nestjs/common';
+import { FieldInterceptor, FilterInterceptor } from '@app/common/interceptors';
 import { AuthGuard, PolicyGuard, ScopeGuard } from '@app/common/guards';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Action, Resource, Scope } from '@wenex/sdk/common';
 import { SetPolicy, SetScope } from '@app/common/metadatas';
 import { SentryInterceptor } from '@ntegral/nestjs-sentry';
 import { AllExceptionsFilter } from '@app/common/filters';
+import { ValidationPipe } from '@app/common/pipes';
 import { Headers } from '@app/common/interfaces';
 import { wrap } from '@app/common/utils';
 
 import { MailsService } from './mails.service';
-import { FieldInterceptor, FilterInterceptor } from '@app/common/interceptors';
 
 @Controller()
+@UsePipes(ValidationPipe)
 @UseFilters(AllExceptionsFilter)
 @UseInterceptors(new SentryInterceptor())
 @UseGuards(AuthGuard, ScopeGuard, PolicyGuard)
@@ -19,8 +27,8 @@ export class MailsController {
   constructor(readonly service: MailsService) {}
 
   @MessagePattern('Before: POST /mails/send')
-  @SetScope(Scope.ReadClientBrokersWallet)
-  @SetPolicy(Action.Read, Resource.ClientBrokersWallet)
+  @SetScope(Scope.SendTouchMails)
+  @SetPolicy(Action.Send, Resource.TouchMails)
   @UseInterceptors(FieldInterceptor, FilterInterceptor)
   async send(@Payload('data') data: any, @Payload('headers') headers: Headers) {
     return wrap(await this.service.send(data, headers), 'end');
