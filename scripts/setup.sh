f9a2918eedd5d7a3438da8c8e266eb3db312ab62
Vahid V. <vhid.vz@gmail.com>
2024-07-17 09:23:44 +0330

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@
 ######################
 
 # Proto
-bash ./script/machine.sh
+bash ./scripts/machine.sh
 if [ $? == 0 ]; then echo "Machine prepared successfully."
 else echo "Preparing machine assets was failed...!"; fi
