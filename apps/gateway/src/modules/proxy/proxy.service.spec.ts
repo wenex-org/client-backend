c502956f64ae013737b0859138df58a37a046d56
Vahid V. <vhid.vz@gmail.com>
2025-01-06 12:03:08 +0330

    update

--- apps/gateway/src/modules/proxy/proxy.service.spec.ts
+++ apps/gateway/src/modules/proxy/proxy.service.spec.ts
@@ -1,15 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProxyService } from './proxy.service';
+import { ProxyModule } from './proxy.module';
 
 describe('ProxyService', () => {
   let service: ProxyService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProxyService],
+      imports: [ProxyModule],
     }).compile();
 
-    service = module.get<ProxyService>(ProxyService);
+    service = await module.resolve<ProxyService>(ProxyService);
   });
 
   it('should be defined', () => {
