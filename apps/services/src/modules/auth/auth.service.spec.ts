f4fd33ef19201f0bea66151d90c6af34d2807f73
Vahid V. <vhid.vz@gmail.com>
2025-01-25 09:20:43 +0330

    update

--- apps/services/src/modules/auth/auth.service.spec.ts
+++ apps/services/src/modules/auth/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { AuthModule } from './auth.module';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -7,7 +8,7 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [AuthModule],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
