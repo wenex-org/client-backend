ff3a9a0a3441ffd4cd5e0c52ab879e9c4b409297
Vahid V. <vhid.vz@gmail.com>
2025-01-27 11:00:53 +0330

    update

--- apps/services/src/modules/touch/submodule/mails/mails.service.ts
+++ apps/services/src/modules/touch/submodule/mails/mails.service.ts
@@ -15,7 +15,7 @@ export class MailsService {
   send(data: Mail, headers?: Headers): Promise<SyncEnd> {
     const { touch } = this.sdkService.client;
     const { template, options, context } = data;
-    const path = `modules/touch/hbs/${toKebabCase(template)}.hbs`;
+    const path = `modules/touch/submodule/mails/hbs/${toKebabCase(template)}.hbs`;
     const file = readFileSync(join(__dirname, path));
     const plate = hbs.compile(file.toString('utf8'), { noEscape: true });
     const html = plate(context);
