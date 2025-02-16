621bd033d33eed34f46b96ebec1a2576205c6331
Vahid V. <vhid.vz@gmail.com>
2025-02-16 13:15:16 +0330

    update

--- apps/services/src/modules/touch/submodule/mails/mails.service.ts
+++ apps/services/src/modules/touch/submodule/mails/mails.service.ts
@@ -3,7 +3,6 @@ import { toKebabCase } from 'naming-conventions-modeler';
 import { SyncEnd } from '@app/common/core/interfaces';
 import { contextGen } from '@app/common/utils/touch';
 import { Mail } from '@app/common/interfaces/touch';
-import { mask } from '@wenex/sdk/common/core/utils';
 import { SdkService } from '@app/module/sdk';
 import { Injectable } from '@nestjs/common';
 import { readFileSync } from 'fs';
@@ -14,18 +13,13 @@ import { join } from 'path';
 export class MailsService {
   constructor(private readonly sdkService: SdkService) {}
 
-  async send(data: Mail, headers?: Headers): Promise<SyncEnd> {
+  send(data: Mail, headers?: Headers): Promise<SyncEnd> {
     const { touch } = this.sdkService.client;
     const { template, options, context } = data;
     const path = `modules/touch/submodule/mails/hbs/${toKebabCase(template)}.hbs`;
     const file = readFileSync(join(__dirname, path));
     const plate = hbs.compile(file.toString('utf8'), { noEscape: true });
     const html = plate(contextGen(template, context ?? {}));
-    options.props = { ...options.props, context: mask([context])[0] };
-    const result = await touch.emails.send({ ...options, html }, { headers });
-
-    // TODO
-
-    return touch.emails.updateById(result.id, { html: html.replace(/\d+/g, '*') }, { headers });
+    return touch.emails.send({ ...options, html }, { headers });
   }
 }
