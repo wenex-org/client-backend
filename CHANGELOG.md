d39befa05bd914e7957ad8af25cac9ba7f06138c
Vahid V. <vhid.vz@gmail.com>
2025-06-02 11:31:20 +0330

    feat: update changelog and version to 1.1.0; add new words to cSpell and refactor CQRS service

---
+++ CHANGELOG.md
@@ -0,0 +1,31 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+
+- conjoint services.
+
+## [1.1.0] - 2025-06-02
+
+### Changed
+
+- refactor: update DB_NAME function to use default prefix and adjust related tests @vhidvz
+- refactor: change collection type in CqrsSourceDto and CqrsSource interface @vhidvz
+- refactor: update BackupService to use new collection structure and improve index management @vhidvz
+- refactor: simplify ensureIndexes function signature for better type handling @vhidvz
+
+## [1.0.X] - 2024-01-01
+
+### Added
+
+- initial release 🎉​🎊​.
+
+[unreleased]: https://github.com/wenex-org/platform-sdk/compare/1.1.0...HEAD
+[1.1.0]: https://github.com/wenex-org/platform-sdk/compare/1.0.21...1.1.0
+[1.0.X]: https://github.com/wenex-org/platform-sdk/releases/tag/1.0.21
--- apps/workers/src/modules/cqrs/cqrs.service.ts
+++ apps/workers/src/modules/cqrs/cqrs.service.ts
@@ -1,4 +1,5 @@
 import { fixIn, ObjectId } from '@app/common/core/utils/mongo';
+import { COLLECTION, Database } from '@wenex/sdk/common/core';
 import { CqrsPayload } from '@app/common/core/interfaces';
 import { logger } from '@wenex/sdk/common/core/utils';
 import { InjectConnection } from '@nestjs/mongoose';
@@ -11,15 +12,16 @@ export class CqrsService {
 
   constructor(@InjectConnection() private readonly conn: Connection) {}
 
-  async cqrs({ id, ...payload }: CqrsPayload) {
-    const { source, after } = payload;
+  async cqrs({ id, source, after }: CqrsPayload) {
+    const { db, collection } = source;
     const query = { _id: ObjectId(id) };
+    const coll = COLLECTION(collection, db.split('-').pop() as Database).replace('/', '.');
     if (after) {
-      await this.conn.collection(source.collection).replaceOne(query, fixIn(after), { upsert: true });
-      this.log.extend(this.cqrs.name)(`Replaced %s document into the %s collection`, id, source.collection);
+      await this.conn.collection(coll).replaceOne(query, fixIn(after), { upsert: true });
+      this.log.extend(this.cqrs.name)(`Replaced %s document into the %s collection`, id, coll);
     } else {
-      await this.conn.collection(source.collection).deleteOne(query);
-      this.log.extend(this.cqrs.name)(`Removed %s document from the %s collection`, id, source.collection);
+      await this.conn.collection(coll).deleteOne(query);
+      this.log.extend(this.cqrs.name)(`Removed %s document from the %s collection`, id, coll);
     }
   }
 }
